5221501ff5ce13d2a42b50c0a7f0e547ebbabaef
Nipun Garg <nipun.garg@kuliza.com>
2018-02-13 16:12:36 +0530

    +base model, +managers

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import MyUserManager
 
 # Create your models here.
 
 
-class MyUser(AbstractBaseUser):
+class BaseModel(models.Model):
+    """
+        This is for storing commonly used fields.
+    """
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now_add=True)
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin, BaseModel):
     """
         This is a User model to create some extra fields.
     """
@@ -12,7 +21,9 @@ class MyUser(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=15)
-    created_on = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
+
+    objects = MyUserManager()
